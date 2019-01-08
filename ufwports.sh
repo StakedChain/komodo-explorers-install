47645db1181e8a237608a5523f24369f2b40e8d8
blackjok3r <blackjok3r@gmail.com>
2019-01-08 12:42:32 +0800

    fix ufw

--- ufwports.sh
+++ ufwports.sh
@@ -2,12 +2,11 @@
 
 CUR_DIR=$(pwd)
 
-rpcport=8232
-zmqport=8332
 webport=3001
 
 ./listassetchains.py | while read i; do
    webport=$((webport+1))
+   echo webport > webport
    daemon_getinfo=$(~/staked/komodo/master/komodo-cli -ac_name=$i getinfo)
    daemon_name=$(echo $daemon_getinfo | jq .name)
    daemon_name=$(echo $daemon_name | tr -d '"')
@@ -15,4 +14,6 @@ webport=3001
    echo "sudo ufw allow $daemon_p2pport/tcp comment '$daemon_name p2p port'"	
 done
 
-echo "# sudo ufw allow from any to any port 3001:$webport proto tcp comment 'allow insight web ports'"
+webport=$(cat webport)
+rm webport
+echo "sudo ufw allow from any to any port 3001:$webport proto tcp comment 'allow insight web ports'"
