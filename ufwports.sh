11073e1dea3a88395302ec2837d441b5ab289bc8
blackjok3r <blackjok3r@gmail.com>
2019-01-08 12:38:49 +0800

    fix

--- ufwports.sh
+++ ufwports.sh
@@ -7,16 +7,11 @@ zmqport=8332
 webport=3001
 
 ./listassetchains.py | while read i; do
-   rpcport=$((rpcport+1))
-   zmqport=$((zmqport+1))
    webport=$((webport+1))
    daemon_getinfo=$(~/staked/komodo/master/komodo-cli -ac_name=$i getinfo)
    daemon_name=$(echo $daemon_getinfo | jq .name)
    daemon_name=$(echo $daemon_name | tr -d '"')
-   daemon_rpcport=$(echo $daemon_getinfo | jq .rpcport)
    daemon_p2pport=$(echo $daemon_getinfo | jq .p2pport)
-   daemon_magic=$(echo $daemon_getinfo | jq .magic)
-   daemon_magic_hex=$(printf '%016x' $daemon_magic)
    echo "sudo ufw allow $daemon_p2pport/tcp comment '$daemon_name p2p port'"	
 done
 
